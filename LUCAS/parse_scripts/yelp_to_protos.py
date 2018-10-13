dd16520b3f8efc15baae80b1e0caf515baf0f82b
Stefan <stefan.kennedy48@mail.dcu.ie>
2018-10-14 00:51:49 +0100

    Added outputting normalised reviews to file

--- LUCAS/parse_scripts/yelp_to_protos.py
+++ LUCAS/parse_scripts/yelp_to_protos.py
@@ -7,6 +7,9 @@ from formatting import format_yelp_nyc_review_content
 yelp_nyc_directory = sys.argv[1]
 
 review_set = review_set_pb2.ReviewSet()
-with open(os.path.normpath(yelp_nyc_directory + '/reviewContent')) as f:
-  format_yelp_nyc_review_content(review_set.reviews.add(), f.readline())
-print (review_set)
+with open(os.path.normpath(yelp_nyc_directory + '/reviewContent'), 'r') as f:
+  for line in f:
+    format_yelp_nyc_review_content(review_set.reviews.add(), f.readline())
+
+with open(os.path.normpath(yelp_nyc_directory + '/normalisedReviewContent'), 'w') as f:
+  f.write(str(review_set))
