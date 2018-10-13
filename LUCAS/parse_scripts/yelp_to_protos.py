94e1d855db2b1a61ce8b183779f9efd5409e3f04
Stefan <stefan.kennedy48@mail.dcu.ie>
2018-10-13 22:04:36 +0100

    Changed yelp to protos runner to use helper func

--- LUCAS/parse_scripts/yelp_to_protos.py
+++ LUCAS/parse_scripts/yelp_to_protos.py
@@ -1,9 +1,9 @@
 import sys
 import os
 from protos import review_pb2
+from formatting import format_yelp_nyc_review_content
 
 yelp_directory = sys.argv[1]
 
 with open(os.path.normpath(yelp_directory + '/reviewContent')) as f:
-  review = review_pb2.Review()
-  review.review_content = f.readline().split("\t")[3]
+  review = format_yelp_nyc_review_content(f.readline())
