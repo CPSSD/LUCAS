daa5b3f16048dbe6cbaf71e4467e00f5a6ad851c
Stefan <stefan.kennedy48@mail.dcu.ie>
2018-10-14 00:04:01 +0100

    Updated formatting to add date, userid and productid

--- LUCAS/parse_scripts/formatting.py
+++ LUCAS/parse_scripts/formatting.py
@@ -2,5 +2,9 @@ from protos import review_pb2
 
 def format_yelp_nyc_review_content(content):
   review = review_pb2.Review()
-  review.review_content = content.split("\t")[3]
+  split_tokens = content.split("\t")
+  review.review_content = split_tokens[3]
+  review.date = split_tokens[2]
+  review.user_id = int(split_tokens[0])
+  review.product_id = int(split_tokens[1])
   return review
--- LUCAS/parse_scripts/formatting_test.py
+++ LUCAS/parse_scripts/formatting_test.py
@@ -1,5 +1,20 @@
 from formatting import format_yelp_nyc_review_content
 
+def _get_entry_text(userid, productid, date, content):
+  return str(userid) + "\t" + str(productid) + "\t" + date + "\t" + content
+
+def test_formatting_yelp_nyc_gives_correct_user_id():
+  entry = _get_entry_text(2, 1, "2000-01-01", "abc")
+  assert format_yelp_nyc_review_content(entry).user_id == 2
+
+def test_formatting_yelp_nyc_gives_correct_product_id():
+  entry = _get_entry_text(1, 3, "2000-01-01", "Blarg")
+  assert format_yelp_nyc_review_content(entry).product_id == 3
+
+def test_formatting_yelp_nyc_gives_correct_date():
+  entry = _get_entry_text(1, 1, "2001-02-03", "abc")
+  assert format_yelp_nyc_review_content(entry).date == "2001-02-03"
+
 def test_formatting_yelp_nyc_gives_correct_review_content():
-  entry = "100000	100	2000-01-01	Blarg"
+  entry = _get_entry_text(1, 1, "2000-01-01", "Blarg")
   assert format_yelp_nyc_review_content(entry).review_content == "Blarg"
