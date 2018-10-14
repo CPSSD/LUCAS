6754e25bdcc58328905dc4dceabe127b1003de12
Stefan <stefan.kennedy48@mail.dcu.ie>
2018-10-14 14:28:22 +0100

    Added finding review set of yelp chi reviews

--- LUCAS/parse_scripts/yelp_to_protos.py
+++ LUCAS/parse_scripts/yelp_to_protos.py
@@ -3,6 +3,10 @@ import os
 from protos import review_pb2
 from protos import review_set_pb2
 from formatting import format_yelp_nyc_review_content
+from formatting import format_yelp_chi_review
+from id_map_service import IDMapService
+
+# NYC
 
 yelp_nyc_directory = sys.argv[1]
 
@@ -11,5 +15,23 @@ with open(os.path.normpath(yelp_nyc_directory + '/reviewContent'), 'r') as f:
   for line in f:
     format_yelp_nyc_review_content(review_set.reviews.add(), f.readline())
 
-with open(os.path.normpath(yelp_nyc_directory + '/normalisedReviewContent'), 'w') as f:
-  f.write(str(review_set))
+ with open(os.path.normpath(yelp_nyc_directory + '/normalisedReviewContent'), 'w') as f:
+   f.write(str(review_set))
+
+# Chicago
+
+yelp_chi_directory = sys.argv[2]
+
+def id_func(map, key):
+  return len(map)
+
+userid_map_service = IDMapService(id_func)
+productid_map_service = IDMapService(id_func)
+
+review_set = review_set_pb2.ReviewSet()
+with open(os.path.normpath(yelp_chi_directory + '/output_review_yelpHotelData_NRYRcleaned.txt'), 'r') as f1:
+  with open(os.path.normpath(yelp_chi_directory + '/output_meta_yelpHotelData_NRYRcleaned.txt'), 'r') as f2:
+    for line in f1:
+      format_yelp_chi_review(review_set.reviews.add(), line, f2.readline(), userid_map_service, productid_map_service)
+
+print (str(review_set))
