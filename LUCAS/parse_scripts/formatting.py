da1ac7a2fec7b6e969853f82c14307360b548edd
Stefan <stefan.kennedy48@mail.dcu.ie>
2018-10-14 14:11:27 +0100

    Added functions for formatting yelp chi data

--- LUCAS/parse_scripts/formatting.py
+++ LUCAS/parse_scripts/formatting.py
@@ -4,3 +4,11 @@ def format_yelp_nyc_review_content(review, content):
   review.date = split_tokens[2]
   review.user_id = int(split_tokens[0])
   review.product_id = int(split_tokens[1])
+
+def format_yelp_chi_review(review, content, metadata, userid_map_service, productid_map_service):
+  review.review_content = content
+
+  split_tokens = metadata.split(" ")
+  review.date = split_tokens[0]
+  review.user_id = userid_map_service.map(split_tokens[2])
+  review.product_id = productid_map_service.map(split_tokens[3])
