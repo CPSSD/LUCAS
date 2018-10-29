0607f1ef5938c5c47eb546fb3a4822f439ce5986
KirillSloka <kirils.sloka2@mail.dcu.ie>
2018-10-29 14:21:51 +0000

    Minor styling updates and added what is it modal

--- Lucify/app/components/header.jsx
+++ Lucify/app/components/header.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { toggleReview, toggleModal } from '../actions/index';
+
 class Header extends React.Component {
+
+  returnToReview() {
+    this.props.toggleReview(false);
+  }
+
+  toggleModal() {
+    this.props.toggleModal();
+  }
+
   render() {
-    return ( 
-    <div className="hero-head">
-      <nav className="navbar has-shadow">
-        <div className="container">
-          <div className="navbar-brand">
-          {this.props.showResults &&
-            <a className="navbar-item title header-title is-rounded is-3" href="/">
-              Lucify
-            </a>
-          }
-          </div>
-          <div id="navbarMenuHeroA" className="navbar-menu">
-            <div className="navbar-end">
-              <a className="navbar-item">
-                What is it?
-              </a>
+    return (
+      <div className="hero-head">
+        <nav className="navbar has-shadow">
+          <div className="container">
+            <div className="navbar-brand">
+              {this.props.showResults &&
+                <a href="#" role="button" className="navbar-item title header-title is-rounded is-3" onClick={() => this.returnToReview()}>
+                  Lucify
+                </a>
+              }
+            </div>
+            <div id="navbarMenuHeroA" className="navbar-menu">
+              <div className="navbar-end">
+                <a className="navbar-item">
+                  <button className="button is-primary" onClick={() => this.toggleModal()}>What is this?</button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
-    )
+        </nav>
+      </div>
+    );
   }
-};
+}
 
 const mapStateToProps = (state) => {
   return { showResults: state.toggleReview };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleReview: (value) => dispatch(toggleReview(value)),
+    toggleModal: () => dispatch(toggleModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
