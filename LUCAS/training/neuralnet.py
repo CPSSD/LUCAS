e72afb0a0f23dcc7f2facefdf539ce6ef3a177f6
Niall Walsh <niallww41@gmail.com>
2018-12-08 21:46:27 +0000

    Basic neural model using a tokenizer and FF nn trained on the OpSpam dataset with 87% accuracy. Incorporated neural model into API, returns class and class probabilities.

---
+++ LUCAS/training/neuralnet.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import keras
+from keras.preprocessing import text
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from scripts import training_helpers
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+from notebooks.experiments import exp4_data_feature_extraction as exp4
+from keras import regularizers
+import gensim
+from gensim.models import KeyedVectors
+from scripts.training_helpers import pickle_model
+
+all_reviews, fake_reviews, genuine_reviews, unused_genuine_reviews = exp4.get_balanced_dataset()
+df = training_helpers.get_data_frame()
+
+X = df['review']
+y = np.array(df['deceptive']).ravel()
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+NUM_WORDS = 2000
+tokenizer = text.Tokenizer(num_words=NUM_WORDS)
+
+tokenizer.fit_on_texts(X)
+pickle_model(tokenizer, 'opspam_tokenizer.pkl')
+
+wv = KeyedVectors.load('../wordvec/yelp_w2v.kv')
+
+def tokenize(data):
+    return tokenizer.texts_to_matrix(data, mode='tfidf')
+
+X = np.array(tokenize(X))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
+
+cvscores = []
+for train, test in kfold.split(X, y):
+    model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(NUM_WORDS,), kernel_regularizer=regularizers.l2(0.01)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(4, activation=tf.nn.relu, kernel_regularizer=regularizers.l2(0.01)),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    ])
+    model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+    model.fit(X[train], y[train], epochs=30, batch_size=16, validation_split=0.3, verbose=1, callbacks=[early_stop])
+    scores = model.evaluate(X[test], y[test], verbose=2)
+    print(model.metrics_names, scores)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    cvscores.append(scores[1] * 100)
+    
+print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+
+final_model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(NUM_WORDS,), kernel_regularizer=regularizers.l2(0.01)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(4, activation=tf.nn.relu, kernel_regularizer=regularizers.l2(0.01)),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+final_model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+history = final_model.fit(X, y, epochs=12, batch_size=16, validation_split=0.3, verbose=1)
+final_model.save('../models/mlp_opspam_86.h5')
+# scores = final_model.evaluate(final_test_x, final_test_y, verbose=2)
+# print(scores)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'b', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'bo', label='Validation loss')
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'ro', label='Validation acc')
+
+plt.title('Training and validation losses and accuracies')
+plt.xlabel('Epochs')
+plt.ylabel('Loss & Acc')
+plt.legend()
+plt.show()
+
+i = input()
+while i is not 'q':
+    seq = tokenize([i])
+    final_model.predict(seq)
+
+
