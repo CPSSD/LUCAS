49b4139e5fd4243f1e13e468644a86d1b1d8ba66
KirillSloka <kirils.sloka2@mail.dcu.ie>
2019-03-31 19:32:42 +0100

    Fix small issue

--- Lucify/app/components/reviews.jsx
+++ Lucify/app/components/reviews.jsx
@@ -297,7 +297,7 @@ class Review extends Component {
       tooltip = (
         <p key={0}>
           {`Average review length is ${Math.floor(reviewLength)} characters`}
-        </p>,
+        </p>
       );
     }
     return (
